d508e51af98219f8597f283513f09abf8125422c
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-06-03 16:47:05 +0530

    added assests and layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import './globals.css';
 import React from 'react';
+import {Inter,Space_Grotesk} from 'next/font/google'
+import { Metadata } from 'next';
+
+
+const inter=Inter({
+  subsets:["latin"],
+  weight:[400,500,600,700],
+  variable:"--font-inter"
+})
+
+const spaceGrotesk=Space_Grotesk({
+  subsets:["latin"],
+  weight:[400,500,600,700],
+  variable:"--font-grotesk"
+})
+
+export const metadata :Metadata = {
+  title:"next 14 with clerk",
+  description:"next 14 with clerk is a great framework for building websites and web applications with react", 
+  icons:{
+    icon:'/assests/images/site-logo.svg'
+  }  
+}
 
 export default function RootLayout({
   children,
@@ -8,18 +31,26 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+    appearance={{
+elements:{
+  formButtonPrimary:"primary-gradient",
+  footerActionLink:"priamry-text-gradient hover:text-primary-500" 
+}
+
+    }}>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <header>
             <SignedOut>
               <SignInButton />
             </SignedOut>
             <SignedIn>
-              <UserButton />
+             
             </SignedIn>
           </header>
           <main>
+            <h1 className='h1-bold'>this is a piece of text</h1>
             {children}
           </main>
         </body>
